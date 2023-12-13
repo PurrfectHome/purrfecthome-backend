722f91731730cbdd69c7224828499b81e6b7dbfa
arvinaufal <arvinaufalagustian@gmail.com>
2023-12-13 23:58:13 +0700

    Add Server: Google Login Schema

--- schemas/user.js
+++ schemas/user.js
@@ -1,5 +1,9 @@
 const { GraphQLError } = require("graphql");
 const User = require("../models/user");
+const { comparePassword } = require("../helpers/bcryptjs");
+const { signToken } = require("../helpers/jwt");
+const { OAuth2Client } = require('google-auth-library');
+const client = new OAuth2Client();
 
 const typeDefs = `#graphql
   type User {
@@ -10,6 +14,7 @@ const typeDefs = `#graphql
     password: String
     long: Float
     lat: Float
+    accountType: String
     createdAt: String
     updatedAt: String
   }
@@ -29,6 +34,12 @@ const typeDefs = `#graphql
     ): User
     login(
         username: String
+        password: String
+    ): User
+    loginGoogle(
+        gToken: String
+        lat: String
+        long: String
         password: String
     ): User
   }
@@ -62,7 +73,79 @@ const resolvers = {
       } catch (err) {
         throw err
       }
-    }
+    },
+
+    login: async (_, { username, password }) => {
+      try {
+        if (!username || username == '') {
+          throw new GraphQLError('Username is required', {
+            extensions: { code: 'Bad Request' },
+          });
+        }
+
+        if (!password || password == '') {
+          throw new GraphQLError('Password is required', {
+            extensions: { code: 'Bad Request' },
+          });
+        }
+        const user = await User.getByUsername({ username });
+
+        if (!user) {
+          throw new GraphQLError('User is not exist', {
+            extensions: { code: 'Not Found' },
+          });
+        }
+
+        const isMatch = comparePassword(password, user.password);
+        if (!isMatch) {
+          throw new GraphQLError('Invalid username/password', {
+            extensions: { code: 'Unauthenticated' },
+          });
+        }
+
+        return { accessToken: signToken({ userId: user._id }) };
+      } catch (err) {
+        throw err;
+      }
+    },
+
+
+    loginGoogle: async (_, { gToken, lat, long }) => {
+      try {
+        const ticket = await client.verifyIdToken({
+          idToken: gToken,
+          audience: process.env.G_CLIENT,
+        });
+
+        const payload = ticket.getPayload();
+        const [user, newUser] = await User.findOrCreate({
+          where: {
+            $or: [
+              { email: payload.email },
+              { username: payload.username, }
+            ]
+
+          },
+          defaults: {
+            fullname: payload.email,
+            email: payload.email,
+            username: payload.email,
+            password: String(Math.random() * 5),
+            long: long,
+            lat: lat,
+            accountType: 'google',
+          },
+        });
+
+        if (newUser) await User.create({ userId: user.id, name: user.username });
+        fullname, username, email, password, longitude, latitude
+        return { accessToken: signToken({ userId: user.id }) };
+      } catch (error) {
+        throw new GraphQLError('Google login failed', {
+          extensions: { code: 'Internal Server Error' },
+        });
+      }
+    },
   }
 }
 
