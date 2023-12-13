dd3155fe261caf38f2529af99dcdfd0a7953e911
arvinaufal <arvinaufalagustian@gmail.com>
2023-12-13 19:43:16 +0700

    Add Server: Config MongoDB, BcryptJS, and JWT helpers

--- config/mongo.js
+++ config/mongo.js
@@ -4,3 +4,21 @@ if (process.env.NODE_ENV !== "production") {
 
 const { MongoClient } = require("mongodb");
 const uri = process.env.DATABASE_URI;
+const dbName = process.env.DATABASE_NAME;
+const client = new MongoClient(uri);
+
+let db;
+async function connect() {
+    try {
+        const database = client.db(dbName);
+        db = database;
+        return database;
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+function getDB() {
+    return db;
+}
+module.exports = { connect, getDB };
